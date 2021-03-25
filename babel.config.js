e972a27d9b88db4565d086377c994510fc4780b9
CaioAugustoV <caioaugustov@gmail.com>
2021-03-25 20:58:18 -0300

    NUR-758 Configuration react navigation

--- babel.config.js
+++ babel.config.js
@@ -19,6 +19,7 @@ module.exports = {
         alias: {
           '@core': './src/core',
           '@modules': './src/modules',
+          '@navigation': './src/navigation',
         },
       },
     ],
